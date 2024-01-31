938db270b61c5b7e3f3853de114c3eaf2aa1e297
humandavey <davey.adams.three@gmail.com>
2024-01-31 12:44:17 -0600

    Table for signups & header
    Co-authored-by: c6ypt0maniac <c6ypt0maniac@users.noreply.github.com>

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,22 @@
+import { Button } from "react-bootstrap";
+import logo from '../imgs/icon.png';
+import { useNavigate } from "react-router-dom";
+
+export default () => {
+    const navigate = useNavigate();
+
+
+    return (
+        <>
+        <div className="header">
+            <img className="logo-icon" src={logo} alt="" onClick={() => navigate("/")}/>
+            <div style={{margin: "auto 0"}}>
+                <h1 className="title-text" onClick={() => navigate("/")}>Sign Up Tech</h1>
+            </div>
+            <div style={{display: "flex"}}>
+                <Button className="login-btn">Login</Button>
+            </div>
+        </div>
+        </>
+    );
+};
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,47 @@
 import { useEffect, useState } from "react";
 import { getNum } from "../firebase";
+import Header from "../components/Header";
+import { Button, Table } from "react-bootstrap";
+
 
 export default () => {
 
-
   return (
     <>
-      <h1>test</h1>
+      <Header />
+      
+      <div className="signup-table">
+        <h2 style={{color: "white"}}>Your sign ups...</h2>
+        <Table
+          bordered
+          hover
+          striped
+        >
+          <thead>
+            <tr>
+              <td>Name</td>
+              <td>Slots Filled</td>
+              <td># of Questions</td>
+              <td>Expiration</td>
+              <td>Settings</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Davey</td>
+              <td>5/58</td>
+              <td>12</td>
+              <td>2/6/2024</td>
+              <td>
+                <Button style={{margin: "0.125rem"}}>✎</Button>
+                <Button style={{margin: "0.125rem"}}>📄</Button>
+                <Button style={{margin: "0.125rem"}}>🔗</Button>
+                <Button style={{margin: "0.125rem"}}>✖</Button>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
     </>
   );
 };
