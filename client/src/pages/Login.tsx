0d806b630c80541ed52db8f80708d0e817adfc7b
c6ypt0maniac <156457402+c6ypt0maniac@users.noreply.github.com>
2024-02-26 12:35:33 -0600

    login fixed

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -9,7 +9,26 @@ const Login = (props) => {
 
     const navigate = useNavigate()
     const onButtonClick = () => {
-
+      if ('' === email) {
+        setEmailError('Please enter your email')
+        return
+      }
+    
+      if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+        setEmailError('Please enter a valid email')
+        return
+      }
+    
+      if ('' === password) {
+        setPasswordError('Please enter a password')
+        return
+      }
+    
+      if (password.length < 7) {
+        setPasswordError('The password must be 8 characters or longer')
+        return
+      }
+    
     }
   };
 export default () => {
